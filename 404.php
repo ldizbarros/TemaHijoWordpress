cc80014582f67aa9c9b7faea9edae20ec575c4df
EC2 Default User <ec2-user@ip-172-31-46-253.us-east-2.compute.internal>
2018-11-26 10:16:21 +0000

    Error 404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link    https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Nikko Portfolio
+ */
+
+get_header(); ?>
+
+<div class="site-content__inner">
+	<h5>UPS! La pagina no existe</h5>
+</div>
+
+<?php get_footer() ?>
